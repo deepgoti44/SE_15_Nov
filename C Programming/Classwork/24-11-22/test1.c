2fc2fed712fa4a86dfb3202787b19086dac0b068
Arvindchocha <109568457+Arvindchocha@users.noreply.github.com>
2022-11-24 11:13:11 +0530

    g

---
+++ C Programming/Classwork/24-11-22/test1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("Welcome to C \t\t\tProgramming.\n");
+	printf("In Tops Technology");
+	printf("\n\n\nTime is 10.30 to 12.00");
+	printf("\nFirst Name : Rakesh");
+}
+
